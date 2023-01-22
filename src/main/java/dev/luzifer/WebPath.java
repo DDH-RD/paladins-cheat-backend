f750c90ed6f7dc4a89b185d7fdc2c8b82151dcda
Luziferium <luziferisdoof@gmail.com>
2023-01-22 07:05:43 +0100

    haha yes, imagine having a memory

--- src/main/java/dev/luzifer/WebPath.java
+++ src/main/java/dev/luzifer/WebPath.java
@@ -5,6 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class WebPath {
 
+    // fun stats wie zb. total played (Maeve : 117), total banned (Vatu : 10239), total scanned matches, total X, total Y
+
     public static final String PALADINS_PATH = "/paladins";
     public static final String GET_EVALUATED_PLAYED_SPECIFIC = PALADINS_PATH + "/get/evaluated/played";
     public static final String GET_EVALUATED_PLAYED_TOTAL = PALADINS_PATH + "/get/evaluated/played/total";
