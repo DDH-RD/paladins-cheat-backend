7623474affa49acd8a55f20a2f0f7515f48c35e1
Luziferium <luziferisdoof@gmail.com>
2023-01-29 01:20:18 +0100

    Run

--- src/main/java/dev/luzifer/spring/MatchController.java
+++ src/main/java/dev/luzifer/spring/MatchController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +17,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.MessageFormat;
 import java.util.Map;
 
-@Controller()
+@RestController
 @RequestMapping(WebPath.MATCH)
 public class MatchController {
 
---
+++ src/main/java/dev/luzifer/spring/PingController.java
@@ -0,0 +1,20 @@
+package dev.luzifer.spring;
+
+import dev.luzifer.Main;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class PingController {
+
+    @GetMapping("ping")
+    public ResponseEntity<String> ping() {
+        Main.LOGGER.info("PING RECEIVED");
+        return ResponseEntity.ok("pong");
+    }
+
+
+}
