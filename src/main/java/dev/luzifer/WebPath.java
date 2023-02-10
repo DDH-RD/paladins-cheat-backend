427a4e972f4c0084bce9201a954376417790bac4
Luziferium <luziferisdoof@gmail.com>
2023-02-10 14:39:54 +0100

    Added count for testing

--- src/main/java/dev/luzifer/WebPath.java
+++ src/main/java/dev/luzifer/WebPath.java
@@ -7,4 +7,6 @@ public class WebPath {
 
     public static final String GAME = "/game";
     public static final String POST = "/post";
+    public static final String GET = "/get";
+    public static final String GET_COUNT = GET + "/count";
 }
