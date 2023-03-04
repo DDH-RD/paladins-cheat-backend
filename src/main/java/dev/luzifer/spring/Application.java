e9f88f1906aab2b1be8112ef5e7c2fb2b4ef7fb0
Luziferium <luziferisdoof@gmail.com>
2023-03-04 04:12:02 +0100

    jasd

--- src/main/java/dev/luzifer/spring/Application.java
+++ src/main/java/dev/luzifer/spring/Application.java
@@ -1,13 +1,8 @@
 package dev.luzifer.spring;
 
-import dev.luzifer.Main;
-import dev.luzifer.data.access.GameDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
