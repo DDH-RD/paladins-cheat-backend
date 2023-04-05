272aedffc2d185d6fe66a099fb58e2382408bbd9
Luziferium <luziferisdoof@gmail.com>
2023-04-05 21:21:35 +0200

    async worker fallback

--- src/main/java/dev/luzifer/data/WallHack.java
+++ src/main/java/dev/luzifer/data/WallHack.java
@@ -1,5 +1,7 @@
 package dev.luzifer.data;
 
+import dev.luzifer.Main;
+
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -12,13 +14,14 @@ public class WallHack {
         WORKER = new Thread(() -> {
             while (true) {
                 try {
-                    if(TASKS.isEmpty()) {
+                    if (TASKS.isEmpty()) {
                         Thread.sleep(2000);
                         continue;
                     }
                     TASKS.poll().run();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    Main.LOGGER.warning("Async Worker interrupted. Trying to restart...");
+                    tryToRestartWorkerAfterCrash();
                 }
             }
         });
@@ -29,5 +32,22 @@ public class WallHack {
 
     public static void submitTask(Runnable runnable) {
         TASKS.add(runnable);
+    }
+
+    private static void tryToRestartWorkerAfterCrash() {
+        int attempts = 0;
+        while (attempts < 30 && !WORKER.isAlive()) {
+            try {
+                WORKER.start();
+                attempts++;
+                Main.LOGGER.info("Trying to restart Async Worker... [" + attempts + "/30]");
+                Thread.sleep(100);
+            } catch (IllegalThreadStateException e) {
+                Main.LOGGER.severe("Failed to restart Async Worker after 30 attempts.");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Main.LOGGER.info("Async Worker state: " + WORKER.isAlive());
     }
 }
