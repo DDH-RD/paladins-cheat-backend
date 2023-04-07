0a8506572db44121866cacd62eef82fbeb3f2a0d
Luziferium <luziferisdoof@gmail.com>
2023-04-07 03:25:53 +0200

    ranked only

--- src/main/java/dev/luzifer/spring/GameController.java
+++ src/main/java/dev/luzifer/spring/GameController.java
@@ -37,56 +37,56 @@ public class GameController {
     }
 
     @GetMapping(WebPath.GET_COUNT)
-    public @ResponseBody ResponseEntity<Integer> count(@RequestParam(required = false) boolean isRanked) {
-        int count = gameDao.count(isRanked);
+    public @ResponseBody ResponseEntity<Integer> count(@RequestParam(required = false) boolean rankedOnly) {
+        int count = gameDao.count(rankedOnly);
         return new ResponseEntity<>(count, HttpStatus.FOUND);
     }
 
     @GetMapping(WebPath.GET_BEST_CHAMP_FOR_MAP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestChampForMap(@RequestParam(required = false) boolean isRanked, @PathVariable String mapName) {
+    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestChampForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName) {
 
         Main.LOGGER.info("EVALUATED BEST CHAMP FOR MAP " + mapName);
         mapName = mapName.replace("_", " ");
 
-        BestChampForMapEvaluation evaluation = new BestChampForMapEvaluation(mapName, gameDao, isRanked);
+        BestChampForMapEvaluation evaluation = new BestChampForMapEvaluation(mapName, gameDao, rankedOnly);
         return new ResponseEntity<>(evaluation.evaluate(), HttpStatus.FOUND);
     }
 
     @GetMapping(WebPath.GET_BEST_CHAMP_OF_CATEGORY_FOR_MAP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestChampOfCategoryForMap(@RequestParam(required = false) boolean isRanked, @PathVariable String mapName, @PathVariable int champCategory) {
+    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestChampOfCategoryForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName, @PathVariable int champCategory) {
 
         Main.LOGGER.info("EVALUATED BEST CHAMP OF CATEGORY " + champCategory + " FOR MAP " + mapName);
         mapName = mapName.replace("_", " ");
 
-        BestChampForMapEvaluation evaluation = new BestChampForMapEvaluation(mapName, gameDao, isRanked);
+        BestChampForMapEvaluation evaluation = new BestChampForMapEvaluation(mapName, gameDao, rankedOnly);
         return new ResponseEntity<>(evaluation.evaluate(champCategory), HttpStatus.FOUND);
     }
 
     @GetMapping(WebPath.GET_BEST_COUNTER_CHAMP_FOR_CHAMP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestCounterChampForChamp(@RequestParam(required = false) boolean isRanked, @PathVariable int champId) {
+    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestCounterChampForChamp(@RequestParam(required = false) boolean rankedOnly, @PathVariable int champId) {
 
         Main.LOGGER.info("EVALUATED BEST COUNTER CHAMP FOR CHAMP " + champId);
 
-        BestCounterChampEvaluation evaluation = new BestCounterChampEvaluation(champId, gameDao, isRanked);
+        BestCounterChampEvaluation evaluation = new BestCounterChampEvaluation(champId, gameDao, rankedOnly);
         return new ResponseEntity<>(evaluation.evaluate(), HttpStatus.FOUND);
     }
 
     @GetMapping(WebPath.GET_BEST_COUNTER_CHAMP_OF_CATEGORY_FOR_CHAMP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestCounterChampOfCategoryForChamp(@RequestParam(required = false) boolean isRanked, @PathVariable int champId, @PathVariable int champCategory) {
+    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestCounterChampOfCategoryForChamp(@RequestParam(required = false) boolean rankedOnly, @PathVariable int champId, @PathVariable int champCategory) {
 
         Main.LOGGER.info("EVALUATED BEST COUNTER CHAMP OF CATEGORY " + champCategory + " FOR CHAMP " + champId);
 
-        BestCounterChampEvaluation evaluation = new BestCounterChampEvaluation(champId, gameDao, isRanked);
+        BestCounterChampEvaluation evaluation = new BestCounterChampEvaluation(champId, gameDao, rankedOnly);
         return new ResponseEntity<>(evaluation.evaluate(champCategory), HttpStatus.FOUND);
     }
 
     @GetMapping(WebPath.GET_BEST_BAN_FOR_MAP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestBanForMap(@RequestParam(required = false) boolean isRanked, @PathVariable String mapName) {
+    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestBanForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName) {
 
         Main.LOGGER.info("EVALUATED BEST BANS FOR MAP " + mapName);
         mapName = mapName.replace("_", " ");
 
-        BestBanForMapEvaluation evaluation = new BestBanForMapEvaluation(mapName, gameDao, isRanked);
+        BestBanForMapEvaluation evaluation = new BestBanForMapEvaluation(mapName, gameDao, rankedOnly);
         return new ResponseEntity<>(evaluation.evaluate(), HttpStatus.FOUND);
     }
 }
