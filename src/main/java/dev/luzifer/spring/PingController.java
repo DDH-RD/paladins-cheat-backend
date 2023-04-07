21d856afd1881e05aae204392c71466ba65584d8
Luziferium <luziferisdoof@gmail.com>
2023-04-07 03:35:39 +0200

    coreeeeeeeeeection

--- src/main/java/dev/luzifer/spring/PingController.java
+++ src/main/java/dev/luzifer/spring/PingController.java
@@ -10,11 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class PingController {
 
-    @RequestMapping("/error")
-    public ResponseEntity<String> error() {
-        return ResponseEntity.badRequest().body("error, bra. 040 or soemthng idk");
-    }
-
     @GetMapping("ping")
     public ResponseEntity<String> ping() {
         Main.LOGGER.info("PING RECEIVED");
