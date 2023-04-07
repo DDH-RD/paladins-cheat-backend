2635b0b0aa52944851d8c02f370908436154f9cd
Luziferium <luziferisdoof@gmail.com>
2023-04-07 04:47:13 +0200

    performance goooes brrrrrrrrrrrrr

--- src/main/java/dev/luzifer/spring/GameController.java
+++ src/main/java/dev/luzifer/spring/GameController.java
@@ -1,8 +1,8 @@
 package dev.luzifer.spring;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import dev.luzifer.Main;
 import dev.luzifer.WebPath;
-import dev.luzifer.data.WallHack;
 import dev.luzifer.data.access.GameDao;
 import dev.luzifer.data.evaluation.BestBanForMapEvaluation;
 import dev.luzifer.data.evaluation.BestChampForMapEvaluation;
@@ -20,12 +20,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 @RestController
 @RequestMapping(WebPath.GAME)
 public class GameController {
+
+    private static final Executor TASK_EXECUTOR =
+            new ThreadPoolExecutor(
+                    Runtime.getRuntime().availableProcessors() / 4,
+                    Runtime.getRuntime().availableProcessors(),
+                    250L,
+                    TimeUnit.MILLISECONDS,
+                    new LinkedBlockingQueue<>(10000),
+                    new ThreadFactoryBuilder()
+                            .setUncaughtExceptionHandler((t, e) -> Main.LOGGER.log(Level.SEVERE, "Uncaught exception in thread " + t.getName(), e))
+                            .setDaemon(true)
+                            .setNameFormat("Paladins Task-%d")
+                            .build(),
+                    new ThreadPoolExecutor.AbortPolicy());
 
     @Autowired
     private GameDao gameDao;
@@ -33,60 +54,90 @@ public class GameController {
     @PostMapping(WebPath.POST)
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void postGames(@RequestBody GameDto[] games) {
-        WallHack.submitTask(() -> gameDao.insert(games));
+        TASK_EXECUTOR.execute(() -> gameDao.insert(games));
     }
 
     @GetMapping(WebPath.GET_COUNT)
-    public @ResponseBody ResponseEntity<Integer> count(@RequestParam(required = false) boolean rankedOnly) {
-        int count = gameDao.count(rankedOnly);
-        return new ResponseEntity<>(count, HttpStatus.FOUND);
+    public @ResponseBody DeferredResult<ResponseEntity<Integer>> count(@RequestParam(required = false) boolean rankedOnly) {
+        DeferredResult<ResponseEntity<Integer>> deferredResult = new DeferredResult<>();
+        CompletableFuture.supplyAsync(() -> gameDao.count(rankedOnly), TASK_EXECUTOR)
+                .thenAccept(count -> deferredResult.setResult(new ResponseEntity<>(count, HttpStatus.FOUND)));
+        return deferredResult;
     }
 
     @GetMapping(WebPath.GET_BEST_CHAMP_FOR_MAP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestChampForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName) {
+    public @ResponseBody DeferredResult<ResponseEntity<Map<Integer, Integer>>> getBestChampForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName) {
 
         Main.LOGGER.info("EVALUATED BEST CHAMP FOR MAP " + mapName);
         mapName = mapName.replace("_", " ");
 
-        BestChampForMapEvaluation evaluation = new BestChampForMapEvaluation(mapName, gameDao, rankedOnly);
-        return new ResponseEntity<>(evaluation.evaluate(), HttpStatus.FOUND);
+        DeferredResult<ResponseEntity<Map<Integer, Integer>>> deferredResult = new DeferredResult<>();
+        String finalMapName = mapName;
+        CompletableFuture.supplyAsync(() -> {
+                    BestChampForMapEvaluation evaluation = new BestChampForMapEvaluation(finalMapName, gameDao, rankedOnly);
+                    return evaluation.evaluate();
+                }, TASK_EXECUTOR)
+                .thenAccept(result -> deferredResult.setResult(new ResponseEntity<>(result, HttpStatus.FOUND)));
+        return deferredResult;
     }
 
     @GetMapping(WebPath.GET_BEST_CHAMP_OF_CATEGORY_FOR_MAP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestChampOfCategoryForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName, @PathVariable int champCategory) {
+    public @ResponseBody DeferredResult<ResponseEntity<Map<Integer, Integer>>> getBestChampOfCategoryForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName, @PathVariable int champCategory) {
 
         Main.LOGGER.info("EVALUATED BEST CHAMP OF CATEGORY " + champCategory + " FOR MAP " + mapName);
         mapName = mapName.replace("_", " ");
 
-        BestChampForMapEvaluation evaluation = new BestChampForMapEvaluation(mapName, gameDao, rankedOnly);
-        return new ResponseEntity<>(evaluation.evaluate(champCategory), HttpStatus.FOUND);
+        DeferredResult<ResponseEntity<Map<Integer, Integer>>> deferredResult = new DeferredResult<>();
+        String finalMapName = mapName;
+        CompletableFuture.supplyAsync(() -> {
+                    BestChampForMapEvaluation evaluation = new BestChampForMapEvaluation(finalMapName, gameDao, rankedOnly);
+                    return evaluation.evaluate(champCategory);
+                }, TASK_EXECUTOR)
+                .thenAccept(result -> deferredResult.setResult(new ResponseEntity<>(result, HttpStatus.FOUND)));
+        return deferredResult;
     }
 
     @GetMapping(WebPath.GET_BEST_COUNTER_CHAMP_FOR_CHAMP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestCounterChampForChamp(@RequestParam(required = false) boolean rankedOnly, @PathVariable int champId) {
+    public @ResponseBody DeferredResult<ResponseEntity<Map<Integer, Integer>>> getBestCounterChampForChamp(@RequestParam(required = false) boolean rankedOnly, @PathVariable int champId) {
 
         Main.LOGGER.info("EVALUATED BEST COUNTER CHAMP FOR CHAMP " + champId);
 
-        BestCounterChampEvaluation evaluation = new BestCounterChampEvaluation(champId, gameDao, rankedOnly);
-        return new ResponseEntity<>(evaluation.evaluate(), HttpStatus.FOUND);
+        DeferredResult<ResponseEntity<Map<Integer, Integer>>> deferredResult = new DeferredResult<>();
+        CompletableFuture.supplyAsync(() -> {
+                    BestCounterChampEvaluation evaluation = new BestCounterChampEvaluation(champId, gameDao, rankedOnly);
+                    return evaluation.evaluate();
+                }, TASK_EXECUTOR)
+                .thenAccept(result -> deferredResult.setResult(new ResponseEntity<>(result, HttpStatus.FOUND)));
+        return deferredResult;
     }
 
     @GetMapping(WebPath.GET_BEST_COUNTER_CHAMP_OF_CATEGORY_FOR_CHAMP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestCounterChampOfCategoryForChamp(@RequestParam(required = false) boolean rankedOnly, @PathVariable int champId, @PathVariable int champCategory) {
+    public @ResponseBody DeferredResult<ResponseEntity<Map<Integer, Integer>>> getBestCounterChampOfCategoryForChamp(@RequestParam(required = false) boolean rankedOnly, @PathVariable int champId, @PathVariable int champCategory) {
 
         Main.LOGGER.info("EVALUATED BEST COUNTER CHAMP OF CATEGORY " + champCategory + " FOR CHAMP " + champId);
 
-        BestCounterChampEvaluation evaluation = new BestCounterChampEvaluation(champId, gameDao, rankedOnly);
-        return new ResponseEntity<>(evaluation.evaluate(champCategory), HttpStatus.FOUND);
+        DeferredResult<ResponseEntity<Map<Integer, Integer>>> deferredResult = new DeferredResult<>();
+        CompletableFuture.supplyAsync(() -> {
+                    BestCounterChampEvaluation evaluation = new BestCounterChampEvaluation(champId, gameDao, rankedOnly);
+                    return evaluation.evaluate(champCategory);
+                }, TASK_EXECUTOR)
+                .thenAccept(result -> deferredResult.setResult(new ResponseEntity<>(result, HttpStatus.FOUND)));
+        return deferredResult;
     }
 
     @GetMapping(WebPath.GET_BEST_BAN_FOR_MAP)
-    public @ResponseBody ResponseEntity<Map<Integer, Integer>> getBestBanForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName) {
+    public @ResponseBody DeferredResult<ResponseEntity<Map<Integer, Integer>>> getBestBanForMap(@RequestParam(required = false) boolean rankedOnly, @PathVariable String mapName) {
 
-        Main.LOGGER.info("EVALUATED BEST BANS FOR MAP " + mapName);
+        Main.LOGGER.info("EVALUATED BEST BAN FOR MAP " + mapName);
         mapName = mapName.replace("_", " ");
 
-        BestBanForMapEvaluation evaluation = new BestBanForMapEvaluation(mapName, gameDao, rankedOnly);
-        return new ResponseEntity<>(evaluation.evaluate(), HttpStatus.FOUND);
+        DeferredResult<ResponseEntity<Map<Integer, Integer>>> deferredResult = new DeferredResult<>();
+        String finalMapName = mapName;
+        CompletableFuture.supplyAsync(() -> {
+                    BestBanForMapEvaluation evaluation = new BestBanForMapEvaluation(finalMapName, gameDao, rankedOnly);
+                    return evaluation.evaluate();
+                }, TASK_EXECUTOR)
+                .thenAccept(result -> deferredResult.setResult(new ResponseEntity<>(result, HttpStatus.FOUND)));
+        return deferredResult;
     }
 }
