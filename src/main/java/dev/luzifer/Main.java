f3b7689be56a000df87ffa063d3bf31c0cab2677
Luziferium <luziferisdoof@gmail.com>
2023-04-12 09:45:49 +0200

    wasdwasd

--- src/main/java/dev/luzifer/Main.java
+++ src/main/java/dev/luzifer/Main.java
@@ -4,15 +4,6 @@ import dev.luzifer.spring.Application;
 import lombok.experimental.UtilityClass;
 import org.springframework.boot.SpringApplication;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.List;
 import java.util.logging.Logger;
 
 @UtilityClass
@@ -21,69 +12,25 @@ public class Main {
     public static final Logger REST_LOGGER = Logger.getLogger("Paladins-REST");
     public static final Logger DATABASE_LOGGER = Logger.getLogger("Paladins-Database");
 
-    private static final File CREDENTIALS_FILE = new File("webservice.properties");
-
-    private static String API_KEY;
-    private static String DATABASE_URL;
-    private static String DATABASE_USERNAME;
-    private static char[] DATABASE_PASSWORD;
-
-    static {
-
-        if(!CREDENTIALS_FILE.exists()) {
-
-            try {
-                CREDENTIALS_FILE.createNewFile();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            try(InputStream input = Main.class.getClassLoader().getResourceAsStream("webservice.properties")) {
-
-                InputStreamReader streamReader = new InputStreamReader(input, StandardCharsets.UTF_8);
-                BufferedReader reader = new BufferedReader(streamReader);
-
-                try(FileWriter fileWriter = new FileWriter(CREDENTIALS_FILE)) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    for (String line; (line = reader.readLine()) != null;) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    fileWriter.write(stringBuilder.toString());
-                }
-            } catch(Exception ignored) {
-            }
-        }
-
-        try {
-            List<String> lines = Files.readAllLines(CREDENTIALS_FILE.toPath());
-            lines.forEach(line -> {
-                if(line.startsWith("api-key:")) API_KEY = line.substring(8);
-                if(line.startsWith("database-url:")) DATABASE_URL = line.substring(13);
-                if(line.startsWith("database-username:")) DATABASE_USERNAME = line.substring(18);
-                if(line.startsWith("database-password:")) DATABASE_PASSWORD = line.substring(18).toCharArray();
-            });
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private static final Credentials CREDENTIALS = Credentials.get();
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 
     public static String getApiKey() {
-        return API_KEY;
+        return CREDENTIALS.getApiKey();
     }
 
     public static String getDatabaseUrl() {
-        return DATABASE_URL;
+        return CREDENTIALS.getDatabaseUrl();
     }
 
     public static String getDatabaseUsername() {
-        return DATABASE_USERNAME;
+        return CREDENTIALS.getDatabaseUsername();
     }
 
     public static char[] getDatabasePassword() {
-        return DATABASE_PASSWORD;
+        return CREDENTIALS.getDatabasePassword();
     }
 }
