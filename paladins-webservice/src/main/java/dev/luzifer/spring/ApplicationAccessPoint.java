4c79d79b390d3fa106a41a72ec26ae4804427195
Luziferium <luziferisdoof@gmail.com>
2023-09-25 15:56:33 +0200

    fixe wixe

--- paladins-webservice/src/main/java/dev/luzifer/spring/ApplicationAccessPoint.java
+++ paladins-webservice/src/main/java/dev/luzifer/spring/ApplicationAccessPoint.java
@@ -7,10 +7,6 @@ public class ApplicationAccessPoint {
 
     public static final String API_KEY = "/{apiKey}"; // /{apiKey}
 
-    // debug
-    public static final String DEBUG = API_KEY + "/debug"; // /{apiKey}/debug
-    public static final String LATEST_LOG_DOWNLOAD = "/latestlog"; // /{apiKey}/debug/latestlog
-
     // game
     public static final String GAME = API_KEY + "/game"; // /{apiKey}/game
     public static final String POST = "/post"; // /{apiKey}/game/post
@@ -25,6 +21,4 @@ public class ApplicationAccessPoint {
     public static final String GET_COUNT_BANNED_CHAMPS = GET_COUNT + "/bannedchamps"; // /{apiKey}/game/get/count/bannedchamps
     public static final String GET_COUNT_REGIONS = GET_COUNT + "/regions"; // /{apiKey}/game/get/count/regions
     public static final String GET_COUNT_MAPS = GET_COUNT + "/maps"; // /{apiKey}/game/get/count/maps
-
-
 }
