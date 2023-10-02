f0a42d941a7346b95dde948389226023bcf3b4d0
Luziferium <luziferisdoof@gmail.com>
2023-10-03 00:22:21 +0200

    nah

--- paladins-webservice/src/main/java/dev/luzifer/spring/ApplicationConfiguration.java
+++ paladins-webservice/src/main/java/dev/luzifer/spring/ApplicationConfiguration.java
@@ -2,20 +2,8 @@ package dev.luzifer.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan({"dev.luzifer.data.access"})
-public class ApplicationConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("http://127.0.0.1:*") // Allow requests from your frontend origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify the allowed HTTP methods
-                .allowCredentials(true); // Allow credentials (cookies, etc.)
-    }
+public class ApplicationConfiguration {
 }
