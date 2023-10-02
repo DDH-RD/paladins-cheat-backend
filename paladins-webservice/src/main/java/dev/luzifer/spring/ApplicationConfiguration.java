94f22a45c2c4f96b2314e3edbfce53d619b2df6e
Luziferium <luziferisdoof@gmail.com>
2023-10-02 23:58:44 +0200

    aksod

--- paladins-webservice/src/main/java/dev/luzifer/spring/ApplicationConfiguration.java
+++ paladins-webservice/src/main/java/dev/luzifer/spring/ApplicationConfiguration.java
@@ -1,9 +1,30 @@
 package dev.luzifer.spring;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @ComponentScan({"dev.luzifer.data.access"})
 public class ApplicationConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*"); // Allow requests from any origin
+        config.addAllowedHeader("*"); // Allow all headers
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
 }
