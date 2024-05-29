6212991664acd88b7d9e6b2278070bde7ab58080
GodCipher <luziferisdoof@gmail.com>
2024-05-29 21:38:54 +0200

    csrf

---
+++ paladins-webservice/src/main/java/dev/luzifer/spring/controller/CsrfController.java
@@ -0,0 +1,14 @@
+package dev.luzifer.spring.controller;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CsrfController {
+
+  @GetMapping("/csrf")
+  public String getCsrfToken(CsrfToken token) {
+    return token.getToken();
+  }
+}
