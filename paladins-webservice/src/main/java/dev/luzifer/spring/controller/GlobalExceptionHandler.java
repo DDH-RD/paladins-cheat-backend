cc9ec6db60909c8104ee5be7509872ded7f343cd
GodCipher <luziferisdoof@gmail.com>
2024-05-29 17:46:59 +0200

    Explicit some logging

--- paladins-webservice/src/main/java/dev/luzifer/spring/controller/GlobalExceptionHandler.java
+++ paladins-webservice/src/main/java/dev/luzifer/spring/controller/GlobalExceptionHandler.java
@@ -5,14 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(RuntimeException.class)
-  public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-    log.error("An error occurred", ex);
-    return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
+    log.error("Failed request: {}", request.getDescription(false), ex);
+    return new ResponseEntity<>("Error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
